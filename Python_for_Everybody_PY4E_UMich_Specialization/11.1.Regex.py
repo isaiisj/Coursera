c33b3482bdbcb97e3272b4ed0046230c34fb192d
Isai <47548422+isaiisj@users.noreply.github.com>
2023-10-24 23:21:13 -0600

    Create 11.1.Regex.py

---
+++ Python_for_Everybody_PY4E_UMich_Specialization/11.1.Regex.py
@@ -0,0 +1,19 @@
+'''In this assignment you will read through and parse a file with text and numbers.
+You will extract all the numbers in the file and compute the sum of the numbers.
+it should work with regex_sum.txt file '''
+import re
+
+name = input("enter the file name: ")
+handle = open(name)
+
+suma = 0
+for line in handle:
+	line = line.rstrip()
+	x = re.findall("[0-9]+",line)
+	if len(x) == 0:
+		continue
+	#print(x)
+	for i in x:
+		suma = suma + int(i)
+	    
+print("The sum is",suma)
