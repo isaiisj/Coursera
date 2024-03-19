027575f4a170a472be03096a8eb1cafc4e06cd3a
Isai <47548422+isaiisj@users.noreply.github.com>
2024-03-19 01:23:55 -0600

    Add files via upload

---
+++ Python_de_cero_a_usuario_UNAM/002_indentado.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Corrige el indentado
+# 
+# Corrige el indentado de las siguientes celdas y ejecuta todas las celdas
+
+# In[2]:
+
+
+# Ejercicio 1, 
+saludo = 'hola '
+nombre  = 'mundo'
+mensaje = saludo + nombre 
+print(mensaje)
+    
+
+
+# In[6]:
+
+
+# Ejercicio 2
+a = 1
+b = 3
+c = a + b
+    
+
+
+
+# In[8]:
+
+
+# Ejercicio 3
+for i in range(5):
+    print(i)
+
+
+
+# In[10]:
+
+
+# Ejercicio 4
+texto = "HOLA MUNDE"
+texto_en_minuscula = texto.lower()
+
+
+
+# In[11]:
+
+
+# Ejercicio 5
+
+numeros = [1, 2, 3, 4, 5]
+total = sum(numeros)
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Python_de_cero_a_usuario_UNAM/004_Variables.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Ejercicio de tipos de variables
+# 
+# Reemplaza las XXX con un elemento que vaya acorde al nombre de la variable.
+# Si hay código extra, no lo borres, solo reemplaza las X por lo que debe
+# ser para lograr el objetivo de cada celda.
+
+# In[1]:
+
+
+# Ejercicio 1:
+flotante    = 2.0
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+# Ejercicio 2
+
+complejo    = 2. + 3j
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+# Ejercicio 3
+
+cadena      = "Hola mundo"
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+# Ejercicio 4
+
+boleano     = True
+
+
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
+# Ejercicio 5
+
+tupla       = (5,'A',True)
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+# Ejercicio 6
+
+lista       = [2,3,True,'A',"Hello"]
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+# Ejercicio 7
+
+diccionario = {"A":1,"kevin":"Alex","B":3}
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+# Ejercicio 8
+# Crea una lista que en el primer elemento tenga una lista [1,2]
+# y en el segundo elemento una tupla ("1","2")
+tupla_lista = [[1,2],("1","2")]
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[13]:
+
+
+# Ejercicio 9 
+# Reemplaza las XXX para crear un diccionario.
+# El diccionario debe tener un elemento  'numeros' que contiene
+# los siguientes números en una lista en este orden 1,2,3,4 
+
+diccionario_lista = {'numeros':[1,2,3,4]}
+
+
+
+# In[ ]:
+
+
+
+
