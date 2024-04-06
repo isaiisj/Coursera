1469ff769704af47d67cd9082adc014ec6220ba1
Isai <47548422+isaiisj@users.noreply.github.com>
2024-04-06 01:59:44 -0600

    Create PrimerCuestionarioPractico.py

---
+++ Introducción_a_la_programación_en_Python_PUC/AprendiendoAProgramarConPython/PrimerCuestionarioPractico.py
@@ -0,0 +1,19 @@
+#1
+def sumador(a, b):
+  # Acá abajo escribe lo que pedimos
+  suma = a + b
+  # No cambies esta línea
+  return suma
+
+#2
+def multiplicacion(a, b):
+  var1 = a + b
+  var2 = a + var1
+  var3 = a * b
+  var4 = a / b
+  
+  # Después de esta línea pon algo como return nombre_variable, 
+  #donde nombre_variable es la variable correcta según lo que pide. 
+  #Ojo: debe escribirse con la misma indentación 
+  #(cantidad de espacios al principio de la línea) que las líneas 2 a 5.
+  return var3
