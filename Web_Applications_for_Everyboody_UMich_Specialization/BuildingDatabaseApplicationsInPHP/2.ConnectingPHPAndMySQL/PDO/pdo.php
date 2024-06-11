43fa67f91fb27651b8d5406aa46570f0da8cc3aa
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-11 05:21:36 -0600

    Update pdo.php

--- Web_Applications_for_Everyboody_UMich_Specialization/BuildingDatabaseApplicationsInPHP/2.ConnectingPHPAndMySQL/PDO/pdo.php
+++ Web_Applications_for_Everyboody_UMich_Specialization/BuildingDatabaseApplicationsInPHP/2.ConnectingPHPAndMySQL/PDO/pdo.php
@@ -2,6 +2,7 @@
 $pdo = new PDO('mysql:host=localhost;port=8889;dbname=misc', 
    'fred', 'zap');
 // See the "errors" folder for details...
+//port=3306 for xampp/linux
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
