2bfa203a2291c75f143ecfa328651bec4b931316
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-13 19:44:16 -0600

    Delete Web_Applications_for_Everyboody_UMich_Specialization/BuildingDatabaseApplicationsInPHP/2.ConnectingPHPAndMySQL/PDO directory

--- Web_Applications_for_Everyboody_UMich_Specialization/BuildingDatabaseApplicationsInPHP/2.ConnectingPHPAndMySQL/PDO/pdo.php
+++
@@ -1,9 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;port=8889;dbname=misc', 
-   'fred', 'zap');
-// See the "errors" folder for details...
-//port=3306 for xampp/linux
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-
