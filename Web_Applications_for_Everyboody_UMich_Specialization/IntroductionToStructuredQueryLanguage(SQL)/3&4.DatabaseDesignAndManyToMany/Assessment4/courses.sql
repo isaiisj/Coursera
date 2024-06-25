0eef2cdc0eabc66cc793def214d15186bb807a0b
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-24 22:27:41 -0600

    Update courses.sql

--- Web_Applications_for_Everyboody_UMich_Specialization/IntroductionToStructuredQueryLanguage(SQL)/3&4.DatabaseDesignAndManyToMany/Assessment4/courses.sql
+++ Web_Applications_for_Everyboody_UMich_Specialization/IntroductionToStructuredQueryLanguage(SQL)/3&4.DatabaseDesignAndManyToMany/Assessment4/courses.sql
@@ -55,3 +55,17 @@ INSERT INTO course (course_id,title) values (NULL,"si206");
 
 
 INSERT INTO member (user_id,course_id,role) VALUES (1,1,1);
+INSERT INTO member (user_id,course_id,role) VALUES (2,1,0);
+INSERT INTO member (user_id,course_id,role) VALUES (3,1,0);
+INSERT INTO member (user_id,course_id,role) VALUES (4,1,0);
+INSERT INTO member (user_id,course_id,role) VALUES (5,1,0);
+INSERT INTO member (user_id,course_id,role) VALUES (6,2,1);
+INSERT INTO member (user_id,course_id,role) VALUES (7,2,0);
+INSERT INTO member (user_id,course_id,role) VALUES (8,2,0);
+INSERT INTO member (user_id,course_id,role) VALUES (9,2,0);
+INSERT INTO member (user_id,course_id,role) VALUES (10,2,0);
+INSERT INTO member (user_id,course_id,role) VALUES (11,3,1);
+INSERT INTO member (user_id,course_id,role) VALUES (12,2,0);
+INSERT INTO member (user_id,course_id,role) VALUES (13,2,0);
+INSERT INTO member (user_id,course_id,role) VALUES (14,2,0);
+INSERT INTO member (user_id,course_id,role) VALUES (15,2,0);
