42337072c75abb09dda3ab4ac2fccc66873a81e4
Isai <47548422+isaiisj@users.noreply.github.com>
2024-07-20 04:35:37 -0600

    Update 10.1.Tuples.py

--- Python_for_Everybody_PY4E_UMich_Specialization/PythonDataStructures/10.1.Tuples.py
+++ Python_for_Everybody_PY4E_UMich_Specialization/PythonDataStructures/10.1.Tuples.py
@@ -1,4 +1,5 @@
-'''Write a program to read through the mbox-short.txt 
+'''
+Write a program to read through the mbox-short.txt 
 and figure out the distribution 
 by hour of the day for each of the messages. 
 You can pull the hour out from the 'From ' line by finding 
