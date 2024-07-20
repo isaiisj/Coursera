7353224e3a6fc7578547a170ae24117e83569412
Isai <47548422+isaiisj@users.noreply.github.com>
2024-07-20 04:41:15 -0600

    Update 7.2.Files_2.py

--- Python_for_Everybody_PY4E_UMich_Specialization/PythonDataStructures/7.2.Files_2.py
+++ Python_for_Everybody_PY4E_UMich_Specialization/PythonDataStructures/7.2.Files_2.py
@@ -1,4 +1,5 @@
-'''Write a program that prompts for a file name, 
+'''
+Write a program that prompts for a file name, 
 then opens that file and reads through the file, 
 looking for lines of the form:
 
@@ -8,7 +9,8 @@ Count these lines and extract the floating point
 values from each of the lines and compute the average of 
 those values and produce an output as shown below. 
 Do not use the sum() function or a variable named sum in your solution.
-when you are testing below enter mbox-short.txt as the file name.'''
+when you are testing below enter mbox-short.txt as the file name.
+'''
 
 # Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
