6f28a7f84a5dff03c1b509207a2a2bb697b96b1c
Isai <47548422+isaiisj@users.noreply.github.com>
2024-07-20 04:45:43 -0600

    Update 8.1.Lists_1.py

--- Python_for_Everybody_PY4E_UMich_Specialization/PythonDataStructures/8.1.Lists_1.py
+++ Python_for_Everybody_PY4E_UMich_Specialization/PythonDataStructures/8.1.Lists_1.py
@@ -1,9 +1,11 @@
-'''Open the file romeo.txt and read it line by line. For each line, 
+'''
+Open the file romeo.txt and read it line by line. For each line, 
 split the line into a list of words using the split() method. The program 
 should build a list of words. For each word on each line check to see if 
 the word is already in the list and if not append it to the list. When 
 the program completes, sort and print the resulting words in python sort() 
-order as shown in the desired output.'''
+order as shown in the desired output.
+'''
 
 fname = input("Enter file name: ")
 fh = open(fname)
@@ -27,7 +29,8 @@ for word in lst2:
 	if word in lst3:
 	    continue
 	else:
-		lst3.append(word)
-		#print(lst2)
+	    lst3.append(word)
+	    #print(lst2)
+
 #print("\n\nPrinting list 3")
 print(lst3)
