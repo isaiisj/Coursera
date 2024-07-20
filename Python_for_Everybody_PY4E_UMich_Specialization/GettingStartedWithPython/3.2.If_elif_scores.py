86d75c23e9a316e15cf0fdd4eab75687df15dca6
Isai <47548422+isaiisj@users.noreply.github.com>
2024-07-20 04:20:24 -0600

    Update 3.2.If_elif_scores.py

--- Python_for_Everybody_PY4E_UMich_Specialization/GettingStartedWithPython/3.2.If_elif_scores.py
+++ Python_for_Everybody_PY4E_UMich_Specialization/GettingStartedWithPython/3.2.If_elif_scores.py
@@ -1,4 +1,5 @@
-'''Write a program to prompt for a score between 0.0 and 1.0. 
+'''
+Write a program to prompt for a score between 0.0 and 1.0. 
 If the score is out of range, print an error. If the score is 
 between 0.0 and 1.0, print a grade using the following table:
 Score Grade
@@ -8,7 +9,8 @@ Score Grade
 >= 0.6 D
 < 0.6 F
 If the user enters a value out of range, print a suitable 
-error message and exit. For the test, enter a score of 0.85.'''
+error message and exit. For the test, enter a score of 0.85.
+'''
 
 score = input("Enter score:")
 try:
