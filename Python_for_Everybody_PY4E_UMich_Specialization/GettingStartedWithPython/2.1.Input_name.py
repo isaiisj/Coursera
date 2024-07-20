1e9e93bf63370071825bb5736093a6e22e73a513
Isai <47548422+isaiisj@users.noreply.github.com>
2024-07-20 04:17:17 -0600

    Update 2.1.Input_name.py

--- Python_for_Everybody_PY4E_UMich_Specialization/GettingStartedWithPython/2.1.Input_name.py
+++ Python_for_Everybody_PY4E_UMich_Specialization/GettingStartedWithPython/2.1.Input_name.py
@@ -3,7 +3,7 @@ Write a program that uses input to prompt a user for
 their name and then welcomes them. Note that input will 
 pop up a dialog box. Enter Sarah in the pop-up box when
 you are prompted so your output will match the desired output.
- '''
+'''
  
 # The code below almost works
 
