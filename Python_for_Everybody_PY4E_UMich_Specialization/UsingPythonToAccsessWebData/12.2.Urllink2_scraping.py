f2d8585900d11a262b5cd88b39f3206299994e9e
Isai <47548422+isaiisj@users.noreply.github.com>
2024-07-23 02:44:53 -0600

    Update 12.2.Urllink2_scraping.py

--- Python_for_Everybody_PY4E_UMich_Specialization/UsingPythonToAccsessWebData/12.2.Urllink2_scraping.py
+++ Python_for_Everybody_PY4E_UMich_Specialization/UsingPythonToAccsessWebData/12.2.Urllink2_scraping.py
@@ -1,6 +1,9 @@
-''' The program will use urllib to read the HTML from the data files below, 
+''' 
+The program will use urllib to read the HTML from the data files below, 
 and parse the data, extracting numbers and compute the sum of the numbers in the file.
-http://py4e-data.dr-chuck.net/comments_42.html '''
+http://py4e-data.dr-chuck.net/comments_42.html 
+'''
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
