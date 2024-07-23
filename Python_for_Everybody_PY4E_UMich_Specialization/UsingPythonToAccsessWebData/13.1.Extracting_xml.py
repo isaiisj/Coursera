575819c215690f2a11a588a8188ef072ae74182f
Isai <47548422+isaiisj@users.noreply.github.com>
2024-07-23 02:49:38 -0600

    Update 13.1.Extracting_xml.py

--- Python_for_Everybody_PY4E_UMich_Specialization/UsingPythonToAccsessWebData/13.1.Extracting_xml.py
+++ Python_for_Everybody_PY4E_UMich_Specialization/UsingPythonToAccsessWebData/13.1.Extracting_xml.py
@@ -1,7 +1,9 @@
-'''The program will prompt for a URL, read the XML 
+'''
+The program will prompt for a URL, read the XML 
 data from that URL using urllib and then parse and 
 extract the comment counts from the XML data, compute
-the sum of the numbers in the file.'''
+the sum of the numbers in the file.
+'''
 
 import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as et
