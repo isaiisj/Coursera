95955347728a66fdc95bfb4bb135895b0d764ec8
Isai <47548422+isaiisj@users.noreply.github.com>
2024-07-23 02:50:29 -0600

    Update 13.2.Json1.py

--- Python_for_Everybody_PY4E_UMich_Specialization/UsingPythonToAccsessWebData/13.2.Json1.py
+++ Python_for_Everybody_PY4E_UMich_Specialization/UsingPythonToAccsessWebData/13.2.Json1.py
@@ -1,7 +1,9 @@
-'''The program will prompt for a URL, read the JSON data from
+'''
+The program will prompt for a URL, read the JSON data from
 that URL using urllib and then parse and extract the comment
 counts from the JSON data, compute the sum of the numbers 
-in the file and enter the sum below:'''
+in the file and enter the sum below:
+'''
 
 import urllib.request, urllib.parse, urllib.error
 import json
