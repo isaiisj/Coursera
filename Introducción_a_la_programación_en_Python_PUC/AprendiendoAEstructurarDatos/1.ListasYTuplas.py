0f42a3b5907d3b07d3ec8dc19cced4607bd3c1cd
Isai <47548422+isaiisj@users.noreply.github.com>
2024-11-26 00:14:02 -0600

    Update 1.ListasYTuplas.py

--- Introducción_a_la_programación_en_Python_PUC/AprendiendoAEstructurarDatos/1.ListasYTuplas.py
+++ Introducción_a_la_programación_en_Python_PUC/AprendiendoAEstructurarDatos/1.ListasYTuplas.py
@@ -1,1 +1,13 @@
+'''
+1.
+Tienes una lista de enteros ordenados de menor a mayor, por ejemplo:
 
+lista = [-40, -1, 1, 5, 16, 72, 100]
+Deberás definir la función agregar_entero(lista, entero), la cual recibe 
+como parámetro una lista en el formato anterior y un entero cualquiera. 
+La función deberá agregar el entero en la posición correcta de la lista 
+y luego retornar la lista. Por ejemplo, si se llama la función: agregar_entero(L, 13), 
+siendo L la lista anterior, entonces deberás retornar la lista:
+
+[-40, -1, 1, 5, 13, 16, 72, 100]
+'''
