449a08dab5c1fc6c6712c877f2602ae78de1dc21
Isai <47548422+isaiisj@users.noreply.github.com>
2025-03-23 02:43:10 -0600

    Create Main.java

---
+++ Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/2.ControlFlowStatementsAndLoops/1.WritingConditionalStatements/Challenge3/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int moneyLeftWithToni = 100;
+        if (moneyLeftWithToni >= 300) {
+            System.out.println("Go to Las Vegas");
+            System.out.println("Enjoy some luck games");
+            System.out.println("Enjoy some concerts");
+            System.out.println("Enjoy fine dining");
+            System.out.println("Go on a helicopter ride");
+            System.out.println("Go shop some souvenirs");
+        }  // Put your else statement here. Advice: Use best practice and use curly braces
+        else{
+            System.out.println("think about the money");
+        }
+        System.out.println("On trip to parents.");
+    }
+}
