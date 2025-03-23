21554bc57093588c5ea7e4142cc3cb102f201159
Isai <47548422+isaiisj@users.noreply.github.com>
2025-03-23 02:29:03 -0600

    Create Main.java

---
+++ Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/2.ControlFlowStatementsAndLoops/1.WritingConditionalStatements/Challenge1/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int moneyLeftWithToni = 300;
+        
+        if (moneyLeftWithToni >= 300) 
+            System.out.println("Go to Las Vegas");
+            System.out.println("Enjoy some luck games");
+            System.out.println("Enjoy some concerts");
+            System.out.println("Enjoy fine dining");
+            System.out.println("Go on a helicopter ride");
+            System.out.println("Go shop some souvenirs");
+        
+        
+        System.out.println("On trip to parents.");
+    }
+}
