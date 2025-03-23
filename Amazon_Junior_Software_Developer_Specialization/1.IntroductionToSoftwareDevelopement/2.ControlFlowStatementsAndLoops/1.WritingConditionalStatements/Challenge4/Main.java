91d53bded23f0eaa0ddac921f733c6243c52652b
Isai <47548422+isaiisj@users.noreply.github.com>
2025-03-23 02:57:53 -0600

    Create Main.java

---
+++ Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/2.ControlFlowStatementsAndLoops/1.WritingConditionalStatements/Challenge4/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        int moneyLeftWithToni = 200;
+        if (moneyLeftWithToni >= 300) {
+            System.out.println("Go to Las Vegas");
+            System.out.println("Enjoy some luck games");
+            System.out.println("Enjoy some concerts");
+            System.out.println("Enjoy fine dining");
+            System.out.println("Go on a helicopter ride");
+            System.out.println("Go shop some souvenirs");
+        }   
+        // Put your else-if statements here.
+        // Advice: Use best practice and use curly braces always.
+        // Tip 1: The 2nd condition needs to be checked for only >=200.
+        // Since it moves 1st else-if only when the value is < 300.
+        // Tip 2: The 3rd condition needs to be checked for only >=100.
+        // Tip 3: The 4th condition needs to be checked for only >=50.
+        // Tip 4: Last condition can be an else. It will cover everything.
+        else if (moneyLeftWithToni >= 200){
+            System.out.println("Go to Hoover Dam");
+        }
+        else if (moneyLeftWithToni >= 100){
+            System.out.println("Go to Area 51");
+        }
+        else if (moneyLeftWithToni >= 50){
+            System.out.println("Have an ice cream");
+        }
+        else if (moneyLeftWithToni < 50){
+            System.out.println("Think about the money spent");
+        }
+
+        System.out.println("On trip to parents.");
+    }
+}
