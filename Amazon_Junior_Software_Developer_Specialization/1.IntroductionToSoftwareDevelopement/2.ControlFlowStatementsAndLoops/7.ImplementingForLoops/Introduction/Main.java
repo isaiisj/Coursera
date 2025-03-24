daebbe0addca1bcfa33ee1263a4c9912c5ec044b
Isai <47548422+isaiisj@users.noreply.github.com>
2025-03-23 22:52:53 -0600

    Create Main.java

---
+++ Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/2.ControlFlowStatementsAndLoops/7.ImplementingForLoops/Introduction/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int scoopsSold;
+        System.out.print("Selling chocolate ice cream: 20 scoops");
+        for(scoopsSold=0; scoopsSold<20; scoopsSold++) {
+            System.out.println("Sold Scoop No: " + (scoopsSold + 1));
+        }
+        System.out.print("No more chocolate scoops left in box.");
+    }
+}
