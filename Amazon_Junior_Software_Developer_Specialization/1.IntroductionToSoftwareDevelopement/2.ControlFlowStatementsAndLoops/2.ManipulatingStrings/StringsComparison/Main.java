bdd905cd76666de710013ad2a0d34df0661d2107
Isai <47548422+isaiisj@users.noreply.github.com>
2025-03-25 01:29:31 -0600

    Create Main.java

---
+++ Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/2.ControlFlowStatementsAndLoops/2.ManipulatingStrings/StringsComparison/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        String name1= "santa";
+        String name2 ="Santa";
+
+        if(name1.equalsIgnoreCase(name2)) {
+            System.out.println("The names are the same.");
+        }
+
+    }
+}
