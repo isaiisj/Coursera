630108c3631626d194d15f3b1b9b66697f6f34d9
Isai <47548422+isaiisj@users.noreply.github.com>
2025-03-27 00:55:42 -0600

    Update Main.java

--- Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/1.GettingStartedWithJava/05.WorkingWithStrings(Lab)/Main.java
+++ Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/1.GettingStartedWithJava/05.WorkingWithStrings(Lab)/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String bookTitle = "The Adventures of Captain Fantastic and the Magical Unicorn";
-        int titleLength = bookTitle.length(); // find out the length of bookTitle
+        int titleLength = bookTitle.length(); // TODO: find out the length of bookTitle
         System.out.println("Length of the book title: " + titleLength);
 
         //Declare and initialise the length of shortened book title
@@ -10,7 +10,7 @@ public class Main {
 
 
         // Shortened the book title
-        String shortBookTitle = bookTitle.substring(0,maxLength);// shorten the bookTitle to 20 characters
+        String shortBookTitle = bookTitle.substring(0,maxLength);// TODO: shorten the bookTitle to 20 characters
         System.out.println("Original title: " + bookTitle);
         System.out.println("Shortened title: " + shortBookTitle);
 
@@ -18,7 +18,7 @@ public class Main {
         String searchWord = "Captain";
 
         // Check if the searchWord is present in book title
-        boolean containsWord = bookTitle.contains(searchWord);// check if searchWord is present in bookTitle.
+        boolean containsWord = bookTitle.contains(searchWord);// TODO: check if searchWord is present in bookTitle.
         System.out.println("Does the title contain the word \"" + searchWord + "\"? " + containsWord);
       
     }
