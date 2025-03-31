c6db2b7ee708ba4a56fa29f15699864b52344707
Isai <47548422+isaiisj@users.noreply.github.com>
2025-03-30 20:31:31 -0600

    Create Main.java

---
+++ Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/3.ObjectOrientedProgrammingBasics/1.WritingMethodSignatures/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+		// Area of a square = side * side
+    static double sideOfSquare;
+
+		// method to set the side of the square
+    public static void setSquareSide() {
+        sideOfSquare = 5.2;
+    }
+
+		// method to calculate the area of the square
+    public static double calculateAreaOfSquare() {
+        return sideOfSquare * sideOfSquare;
+    }
+
+		// the main method
+    public static void main(String[] args) {
+				// set the side of square
+        setSquareSide();
+
+				// call the method to calculate the area of the square
+				// And store the result given back by the method in a variable
+        double areaOfSquare = calculateAreaOfSquare();
+
+				// print the result, using the values from the variables
+        System.out.println("Area of the square with side " + sideOfSquare + " is: " + areaOfSquare);
+    }
+}
