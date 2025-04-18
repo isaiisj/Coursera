ac4f601d641d649d1ab9d7d0ae6e117e7165c419
Isai <47548422+isaiisj@users.noreply.github.com>
2025-04-18 00:55:21 -0600

    Create Circle.java

---
+++ Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/3.ObjectOrientedProgrammingBasics/7.WorkingWithClassesObjectsAndMethods(Lab)/Circle.java
@@ -0,0 +1,16 @@
+// TODO 5: Create new java class named Circle
+public class Circle {
+    // TODO 6: Add properties to the circle class
+    double radius;
+
+    // TODO 7: Add constructors to circle class No-argument and
+    // parametrized constructor
+    public Circle() {
+        this.radius = 1.0;
+    }
+
+    // Parameterized constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+}
