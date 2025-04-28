39377fd2a403d217ed9ace8b73bc44bcb5a6bf32
Isai <47548422+isaiisj@users.noreply.github.com>
2025-04-28 01:03:39 -0600

    Create Main.java

---
+++ Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/3.ObjectOrientedProgrammingBasics/11.PolymorphismOverloadingAndOverriding/AnotherDimensionToPolymorphism/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+   public static void main(String[] args) {
+
+      // Camera reference referring to SmartCamera object
+       Camera newCamera = new SmartCamera();
+       newCamera.takePicture();    // calls the takePicture() method of SmartCamera
+   }
+}
