faed771d8fb72efd37e4d11b8014ecea46613b4a
Isai <47548422+isaiisj@users.noreply.github.com>
2025-04-30 01:20:24 -0600

    Create CoffeeMaker.java

---
+++ Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/3.ObjectOrientedProgrammingBasics/12.UsingLogicalDesign(Lab)/CoffeeMaker.java
@@ -0,0 +1,12 @@
+// TODO 21: create the CoffeMaker class
+public class CoffeeMaker {
+
+    // TODO 22: create a void method that takes a Coffee object as an argument
+    public void prepareCoffe(Coffee myFavoriteCoffee) {
+        // TODO 23: call the grindBeans() method on the Coffee object
+        myFavoriteCoffee.grindBeans();
+
+        // TODO 24: call the brewCoffee() method on the Coffee object
+        myFavoriteCoffee.brewCoffee();
+    }
+}
