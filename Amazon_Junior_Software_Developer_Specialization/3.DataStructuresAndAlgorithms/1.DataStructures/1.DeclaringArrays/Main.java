36a0a7711057bbc990cf9ef7ae7686bb521afbdc
Isai <47548422+isaiisj@users.noreply.github.com>
2025-04-29 22:52:14 -0600

    Update Main.java

--- Amazon_Junior_Software_Developer_Specialization/3.DataStructuresAndAlgorithms/1.DataStructures/1.DeclaringArrays/Main.java
+++ Amazon_Junior_Software_Developer_Specialization/3.DataStructuresAndAlgorithms/1.DataStructures/1.DeclaringArrays/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args){
+        // Step 1: Declare an array
         int[] daysInMonth = new int[12];
-
+        // Step 2: Dynamically initialize the array
         daysInMonth[0] = 31;
         daysInMonth[1] = 28;
         daysInMonth[2] = 31;
@@ -14,10 +15,10 @@ public class Main {
         daysInMonth[9] = 31;
         daysInMonth[10] = 30;
         daysInMonth[11] = 31;
-
+        // Step 3: Declare an array with the names of the months
         String[] monthNames = new String[]{"Jan","Feb","Mar","Apr",
         "May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
-
+        // Step 4: Print the number of days in each month
         for(int i = 0; i < monthNames.length; i++){
             System.out.println(monthNames[i] + ": " + daysInMonth[i] + " days");
         }
