ed29cb7d52353f34df60f148654cd8f4cfc5f252
Isai <47548422+isaiisj@users.noreply.github.com>
2025-05-05 01:52:43 -0600

    Create KungFuSkills.java

---
+++ Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/3.ObjectOrientedProgrammingBasics/15.ImplementMultipleInheritance(Lab)/KungFuSkills.java
@@ -0,0 +1,9 @@
+/** TODO 3: Create an interface named "KungFuSkills" **/
+public interface KungFuSkills {
+    /** TODO 4: In the interface "KungFuSkills" created in TODO 5,
+     *          get inside the curly braces of the interface  in order
+     *          to declare a method named "kungFuProtection()".
+     *          Ensure that the  return type of the method is "void".
+     **/
+    public void KungFuProtection();
+}
