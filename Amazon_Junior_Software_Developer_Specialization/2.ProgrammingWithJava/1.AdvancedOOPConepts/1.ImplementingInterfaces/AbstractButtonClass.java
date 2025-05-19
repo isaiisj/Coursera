ab6729fb42f0eb39eae850949c9105b004526ec0
Isai <47548422+isaiisj@users.noreply.github.com>
2025-05-18 22:40:12 -0600

    Create AbstractButtonClass.java

---
+++ Amazon_Junior_Software_Developer_Specialization/2.ProgrammingWithJava/1.AdvancedOOPConepts/1.ImplementingInterfaces/AbstractButtonClass.java
@@ -0,0 +1,11 @@
+abstract class AbstractButtonClass {
+    String caption;
+
+    public void SetCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getCaption() {
+        return this.caption;
+    }
+}
