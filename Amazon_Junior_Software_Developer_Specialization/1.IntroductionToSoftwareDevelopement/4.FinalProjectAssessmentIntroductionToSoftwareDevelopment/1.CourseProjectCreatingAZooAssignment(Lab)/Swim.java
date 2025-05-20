529d64295d1c80519c9aaf27f458bfbe7df003b8
Isai <47548422+isaiisj@users.noreply.github.com>
2025-05-20 04:14:26 -0600

    Create Swim.java

---
+++ Amazon_Junior_Software_Developer_Specialization/1.IntroductionToSoftwareDevelopement/4.FinalProjectAssessmentIntroductionToSoftwareDevelopment/1.CourseProjectCreatingAZooAssignment(Lab)/Swim.java
@@ -0,0 +1,8 @@
+/** TODO 6: create a new interface named "Swim"
+ *          and declaring a method inside it
+ *          named "swimming" with the return type
+ *          "void"
+ **/
+public interface Swim {
+    public void swimming();
+}
