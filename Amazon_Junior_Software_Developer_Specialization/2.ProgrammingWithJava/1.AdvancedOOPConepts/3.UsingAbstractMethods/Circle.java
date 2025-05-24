7421c123e6b6c1cd46dcbf21efa0572b4e77a67e
Isai <47548422+isaiisj@users.noreply.github.com>
2025-05-23 23:06:48 -0600

    Create Circle.java

---
+++ Amazon_Junior_Software_Developer_Specialization/2.ProgrammingWithJava/1.AdvancedOOPConepts/3.UsingAbstractMethods/Circle.java
@@ -0,0 +1,13 @@
+class Circle extends Shape {
+    private int radius;
+
+    Circle(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    void area() {
+        double areaOfCircle = Math.PI * Math.pow(this.radius, 2);
+        System.out.println("Area of Circle: " + areaOfCircle);
+    }
+}
