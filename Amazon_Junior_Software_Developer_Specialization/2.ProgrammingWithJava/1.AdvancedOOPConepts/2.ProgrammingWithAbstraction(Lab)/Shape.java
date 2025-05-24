7764f4f4fe768b4bc257f6d13ff4b362bd43e196
Isai <47548422+isaiisj@users.noreply.github.com>
2025-05-23 22:27:09 -0600

    Create Shape.java

---
+++ Amazon_Junior_Software_Developer_Specialization/2.ProgrammingWithJava/1.AdvancedOOPConepts/2.ProgrammingWithAbstraction(Lab)/Shape.java
@@ -0,0 +1,17 @@
+/** TODO 1: Create a shape abstract class
+ *  with the abstract mehtods
+ *  draw() and calculateArea()
+ *  and no instance variables and
+ *  a third method lineColor as
+ *  you want to use it.
+ */
+abstract class Shape {
+    public abstract float calculateArea();
+
+    public abstract float calculatePerimeter();
+
+    public abstract void draw();
+
+    public abstract void lineColor();
+
+}
