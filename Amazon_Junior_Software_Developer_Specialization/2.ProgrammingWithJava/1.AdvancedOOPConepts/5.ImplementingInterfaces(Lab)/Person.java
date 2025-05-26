b68551ca97c76002380cce4837737d0d6e673623
Isai <47548422+isaiisj@users.noreply.github.com>
2025-05-25 21:42:16 -0600

    Create Person.java

---
+++ Amazon_Junior_Software_Developer_Specialization/2.ProgrammingWithJava/1.AdvancedOOPConepts/5.ImplementingInterfaces(Lab)/Person.java
@@ -0,0 +1,10 @@
+abstract class Person {
+    //TODO 1: declare Person attributes name and age here
+    protected int age;
+    protected String name;
+    void getDetails() {
+        //TODO 2: print name and age
+        System.out.println("Name: " + this.name );
+        System.out.println("Age: " + this.age);
+    }
+}
