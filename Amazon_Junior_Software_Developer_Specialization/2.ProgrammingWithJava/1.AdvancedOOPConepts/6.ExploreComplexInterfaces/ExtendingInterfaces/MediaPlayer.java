1e78760dfd39f423b5ca159e7a2041542b643a81
Isai <47548422+isaiisj@users.noreply.github.com>
2025-05-28 01:06:44 -0600

    Create MediaPlayer.java

---
+++ Amazon_Junior_Software_Developer_Specialization/2.ProgrammingWithJava/1.AdvancedOOPConepts/6.ExploreComplexInterfaces/ExtendingInterfaces/MediaPlayer.java
@@ -0,0 +1,23 @@
+class MediaPlayer implements MediaController {
+
+    public void start() {
+        System.out.println("Player has started");
+    }
+
+    public void pause() {
+        System.out.println("player paused");
+    }
+
+    public void stop() {
+        System.out.println("player stopped");
+    }
+
+    public void increase(int value) {
+        System.out.println("volume level increased to "+value+"");
+    }
+
+    public void decrease(int value) {
+        System.out.println("volume level decreased to "+value+"");
+    }
+
+}
