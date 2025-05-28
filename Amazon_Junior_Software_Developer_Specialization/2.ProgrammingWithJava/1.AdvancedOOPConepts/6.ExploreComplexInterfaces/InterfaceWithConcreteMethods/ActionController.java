17ac406dbb0f02362a6bdd50bf0c8c636b754820
Isai <47548422+isaiisj@users.noreply.github.com>
2025-05-28 01:20:42 -0600

    Create ActionController.java

---
+++ Amazon_Junior_Software_Developer_Specialization/2.ProgrammingWithJava/1.AdvancedOOPConepts/6.ExploreComplexInterfaces/InterfaceWithConcreteMethods/ActionController.java
@@ -0,0 +1,13 @@
+interface ActionController {
+    void start();
+    void pause();
+    void stop();
+
+    default void forward() {
+        System.out.println("skip forward by 10 sec");
+    }
+
+    default void backward() {
+        System.out.println("skip backward by 10 sec");
+    }
+}
