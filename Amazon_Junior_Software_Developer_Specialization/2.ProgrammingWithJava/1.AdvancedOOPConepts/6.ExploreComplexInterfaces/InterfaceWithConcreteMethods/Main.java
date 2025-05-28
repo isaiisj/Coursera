2c2b50d99e06c0a47ac140f4e557819666fc3c52
Isai <47548422+isaiisj@users.noreply.github.com>
2025-05-28 01:21:55 -0600

    Create Main.java

---
+++ Amazon_Junior_Software_Developer_Specialization/2.ProgrammingWithJava/1.AdvancedOOPConepts/6.ExploreComplexInterfaces/InterfaceWithConcreteMethods/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        MediaPlayer player = new MediaPlayer();
+        player.start();
+        player.pause();
+        player.forward();
+        player.backward();
+    }
+}
