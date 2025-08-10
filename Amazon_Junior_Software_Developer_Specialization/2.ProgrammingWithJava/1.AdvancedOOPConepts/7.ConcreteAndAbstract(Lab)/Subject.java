ebd5f3c43dfbfe2efbe2f2776aacf1635c53f596
Isai <47548422+isaiisj@users.noreply.github.com>
2025-08-10 16:18:15 -0600

    Create Subject.java

---
+++ Amazon_Junior_Software_Developer_Specialization/2.ProgrammingWithJava/1.AdvancedOOPConepts/7.ConcreteAndAbstract(Lab)/Subject.java
@@ -0,0 +1,19 @@
+class Subject {
+
+    String title;
+    double credits;
+
+    Subject(String title, double credits) {
+        // TODO 4 Include the instance variables -
+        //  title(a string) and credits(double) and
+        //  define the class constructor
+        this.title = title;
+        this.credits = credits;
+    }
+
+    public String toString() {
+        //TODO 5 Define toString() method,
+        // return string representation of the Subject object
+        return "Subject: " + this.title + " Credidts: " + this.credits;
+    }
+}
