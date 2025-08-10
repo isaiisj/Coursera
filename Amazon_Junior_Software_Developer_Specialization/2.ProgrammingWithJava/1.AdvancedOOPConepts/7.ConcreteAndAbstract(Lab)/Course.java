6ad0c117a00a29d8ddb8ee27c1d729173c8ac599
Isai <47548422+isaiisj@users.noreply.github.com>
2025-08-10 16:17:32 -0600

    Create Course.java

---
+++ Amazon_Junior_Software_Developer_Specialization/2.ProgrammingWithJava/1.AdvancedOOPConepts/7.ConcreteAndAbstract(Lab)/Course.java
@@ -0,0 +1,16 @@
+abstract class Course {
+  
+    //TODO 3: include instance variables for Course class and complete constructor code
+    Subject subject;
+    String instructor;
+    int courseFee;
+    int assignmentMarks;
+    int quizMarks;
+
+    Course(Subject subject, String instructor, int fee) {
+        this.subject = subject;
+        this.instructor = instructor;
+        this.courseFee = fee;
+    }
+
+}
