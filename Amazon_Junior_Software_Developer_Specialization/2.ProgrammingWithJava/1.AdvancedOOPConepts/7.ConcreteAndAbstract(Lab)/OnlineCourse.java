d0012de3424007b6eedbb29aa890612198240046
Isai <47548422+isaiisj@users.noreply.github.com>
2025-08-10 16:16:50 -0600

    Create OnlineCourse.java

---
+++ Amazon_Junior_Software_Developer_Specialization/2.ProgrammingWithJava/1.AdvancedOOPConepts/7.ConcreteAndAbstract(Lab)/OnlineCourse.java
@@ -0,0 +1,13 @@
+class OnlineCourse extends Course  {
+  
+    //TODO 15: include videoLessons and weeks attributes
+    int lessons;
+    int weeks;
+
+    OnlineCourse(Subject subject, String instructor, int fee, int lessons, int weeks) {
+        super(subject, instructor, fee);
+        //TODO 16: initialize other attributes
+        this.lessons = lessons;
+        this.weeks = weeks;
+    }
+}
