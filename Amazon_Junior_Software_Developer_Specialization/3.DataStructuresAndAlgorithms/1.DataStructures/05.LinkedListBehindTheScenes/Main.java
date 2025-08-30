540e1756e785f090a46c1cead395449e22d7379d
Isai <47548422+isaiisj@users.noreply.github.com>
2025-08-30 16:48:44 -0600

    Update Main.java

--- Amazon_Junior_Software_Developer_Specialization/3.DataStructuresAndAlgorithms/1.DataStructures/05.LinkedListBehindTheScenes/Main.java
+++ Amazon_Junior_Software_Developer_Specialization/3.DataStructuresAndAlgorithms/1.DataStructures/05.LinkedListBehindTheScenes/Main.java
@@ -18,3 +18,67 @@ public class Main {
     waitlist.printList();
   }
 }
+
+
+Private static class Node {
+  
+  String name;
+  String details;
+  Node next;
+  
+  Node(String name, String details) {
+    this.name = name;
+    this.details = details;
+    this.next = null;
+  }
+}
+
+
+class LinkedList {
+  
+  Node head;
+  LinkedList() {
+    this.head = null;
+  }
+  
+  // Method to add a regular customer to the end of the list
+  void addCustomer(String name, String details) {
+    Node newNode = new Node(name, details);
+    if (head == null) {
+      head = newNode;
+    } else {
+      Node current = head;
+      while (current.next != null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+  
+  // Method to add a VIP customer at the beginning of the list
+  void addVIPCustomer(String name, String details) {
+  
+    //Step 1: Create the new node the head
+    Node newNode = new Node(name, details);
+  
+    //Step 2: Point it to the current head of the list.
+    newNode.next = head;
+  
+    // Step 3: Update the Head to the New Node
+    head = newNode;
+  }
+  
+  // Method to print the linked list
+  void printList() {
+    Node current = head;
+    while (current != null) {
+      System.out.print(current.name + " (" + current.details +")");
+      current = current.next;
+      if (current != null) {
+        System.out.print(" -> ");
+      }
+    }
+    System.out.println(" -> null");
+  }
+  
+}
